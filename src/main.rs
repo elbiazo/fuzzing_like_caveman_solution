3f7a9f7058370ba20a3d95cf6d1f44109dac7415
elbiazo <eric.l.biazo@gmail.com>
2021-11-07 18:26:42 -0500

    adding thread support

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::io;
 use std::os::unix::process::ExitStatusExt;
 use std::process::{Command, ExitStatus};
-
-// use std::sync::Arc;
-// use std::sync::atomic::{AtomicUsize, Ordering};
-// use std::time::Duration;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::time::Duration;
 mod lib;
 
-// const BATCH_SIZE: usize = 100;
+const BATCH_SIZE: usize = 100;
 
-// #[derive(Default)]
-// struct Statistics {
-//     fuzz_cases: AtomicUsize
-// }
+#[derive(Default)]
+struct Statistics {
+    fuzz_cases: AtomicUsize,
+    num_crashes: AtomicUsize
+}
 
 fn fuzz(thr_id: usize, filename: &str, inp: &Vec<u8>) -> io::Result<ExitStatus> {
     // Write out the input to a temporary file
@@ -24,60 +24,53 @@ fn fuzz(thr_id: usize, filename: &str, inp: &Vec<u8>) -> io::Result<ExitStatus> 
     Ok(runner.status)
 }
 
-// fn worker(thr_id: usize, stats: Arc<Statistics>) -> io::Result<()>
-// {
-//     loop {
-//         for _ in 0..BATCH_SIZE{
-//             let filename = "Canon_40D.jpg";
-//             let file_content = std::fs::read(filename).unwrap();
+fn worker(thr_id: usize, stats: Arc<Statistics>) -> io::Result<()> {
+    loop {
+        for _ in 0..BATCH_SIZE {
+            let filename = "Canon_40D.jpg";
 
-//             fuzz(thr_id, filename, &file_content)?;
-//         }
-//         stats.fuzz_cases.fetch_add(BATCH_SIZE, Ordering::SeqCst);
-//     }
-// }
+            let input = std::fs::read(filename).unwrap();
+            let mut mutator = lib::Mutator::new(
+                input,
+                0x2839839283234 ^ unsafe { std::arch::x86_64::_rdtsc() },
+            );
+            mutator.bitflip(0.01);
+            let exit = fuzz(thr_id, filename, &mutator.input).unwrap();
+            if exit.signal() == Some(11) {
+                // std::fs::write(
+                //     format!("./output/crash_{}_{}", stats.num_crashes.load(Ordering::SeqCst), filename),
+                //     mutator.input,
+                // )
+                // .unwrap();
+                stats.num_crashes.fetch_add(1, Ordering::SeqCst);
+            }
+        }
+        stats.fuzz_cases.fetch_add(BATCH_SIZE, Ordering::SeqCst);
+    }
+}
 
 fn main() {
-    // let mut threads = Vec::new();
-    // let stat = Arc::new(Statistics::default());
+    let mut threads = Vec::new();
+    let stat = Arc::new(Statistics::default());
+    stat.num_crashes.fetch_add(0, Ordering::SeqCst);
 
-    // for thr_id in 0..4 {
-    //     let stat = stat.clone();
-    //     threads.push(std::thread::spawn(move || worker(thr_id, stat)));
-    // }
+    for thr_id in 0..4 {
+        let stat = stat.clone();
+        threads.push(std::thread::spawn(move || worker(thr_id, stat)));
+    }
 
-    // let start = std::time::Instant::now();
+    let start = std::time::Instant::now();
 
-    // loop{
-    //     std::thread::sleep(Duration::from_millis(1000));
-    //     let elapsed = start.elapsed().as_secs_f64();
-    //     let cases = stat.fuzz_cases.load(Ordering::SeqCst);
-
-    //     println!("{:10.6} Cases: {:10} | fcps: {:10.2}", elapsed, cases, cases as f64 /elapsed);
-    // }
-    let filename = "Canon_40D.jpg";
-    let mut iter = 0;
     loop {
-        if iter % 10000 == 0 {
-            println!("Iteration: {}", iter);
-        }
-        let input = std::fs::read(filename).unwrap();
-        let mut mutator = lib::Mutator::new(
-            input,
-            0x2839839283234 ^ unsafe { std::arch::x86_64::_rdtsc() },
+        std::thread::sleep(Duration::from_millis(1000));
+        let elapsed = start.elapsed().as_secs_f64();
+        let cases = stat.fuzz_cases.load(Ordering::SeqCst);
+
+        println!(
+            "{:10.6} Cases: {:10} | fcps: {:10.2}",
+            elapsed,
+            cases,
+            cases as f64 / elapsed
         );
-        mutator.bitflip(0.01);
-        let exit = fuzz(0, filename, &mutator.input).unwrap();
-        if exit.signal() == Some(11) {
-            println!("Iteration: {}", iter);
-            println!("Bug");
-            std::fs::write(
-                format!("./output/crash_{}_{}", iter, filename),
-                mutator.input,
-            )
-            .unwrap();
-            break;
-        }
-        iter += 1;
     }
 }
